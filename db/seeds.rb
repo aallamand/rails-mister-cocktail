06fb0752cbdf45776db37c54bfe337d70bd838e6
Arthur Allamand <a.allamand@gmail.com>
2017-08-10 13:14:29 +0200

    base est seedé

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+json = open("http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read
+parsed_json = JSON.parse(json)
+parsed_json.each do |key, value|
+  value.each do |e|
+    e.each do |key, value|
+      ingredient = Ingredient.new(name: value)
+      ingredient.save
+    end
+  end
+end
