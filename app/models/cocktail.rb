7b102cb3aa78e95904a9a93a993b9b30f39a5cf5
Arthur Allamand <a.allamand@gmail.com>
2017-08-11 17:33:05 +0200

    correction

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,4 +3,8 @@ class Cocktail < ApplicationRecord
   has_many :ingredients, through: :doses
   validates :name, presence: true, uniqueness: true
   mount_uploader :photo, PhotoUploader
+  include RailsSortable::Model
+  set_sortable :sort  # indicate sort column
+  # If you do not want timestamps to be updated on sorting, use following option.
+  # set_sortable :sort, silence_recording_timestamps: true
 end
