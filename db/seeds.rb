16b431c09f55ad4924aa88e03bc278a2bd7cc178
Arthur Allamand <a.allamand@gmail.com>
2017-08-11 16:44:49 +0200

    seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,24 @@ parsed_json = JSON.parse(json)
 parsed_json["drinks"].each do |ingredient|
   Ingredient.create(name: ingredient['strIngredient1'])
 end
+
+100.times do
+  url_cocktail = open('http://www.thecocktaildb.com/api/json/v1/1/random.php').read
+  parsed_cocktail = JSON.parse(url_cocktail)
+  parsed_cocktail["drinks"].each do |information|
+    url = information["strDrinkThumb"]
+    cocktail = Cocktail.new(name: information['strDrink'])
+    cocktail.remote_photo_url = url
+    cocktail.save
+    i = 1
+    while i < 15 do
+      ingredient_recipe = Ingredient.find_by name: information["strIngredient#{i}"]
+      ingredient_recipe
+      dose = Dose.new(description: information["strMeasure#{i}"])
+      dose.ingredient = ingredient_recipe
+      dose.cocktail = cocktail
+      dose.save
+      i += 1
+    end
+  end
+end
